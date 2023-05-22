d50f6676de913abd1ffcb831214936f95c5afdab
Logon Aluno <logonrmlocal@fiap.com.br>
2023-05-22 13:43:15 -0300

    REVISÃO BOOTSTRAP EM REACT

---
+++ react/revBoot/src/App.jsx
@@ -0,0 +1,15 @@
+import  Button from 'react-bootstrap/Button';
+import NavProjeto from './components/NavProjeto';
+import Carrossel from './components/Carrossel';
+function App() {
+
+  return (
+    <div>
+      <NavProjeto/>
+      <h2>Exemplo</h2>
+      <Button variant="outline-secondary" size="lg"> BOTÃO </Button>{' '}
+    </div>
+  )
+}
+
+export default App;
